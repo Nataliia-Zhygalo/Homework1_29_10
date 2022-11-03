13e40d575a7a39c1ac37886e3fe2be72d83d12a9
Nataliia <109826219+Nataliia-Zhygalo@users.noreply.github.com>
2022-11-03 07:36:10 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+num1= parseInt(prompt ("Type in your first number"));
+num2 = parseInt(prompt ("Type in your second number"));
+console.log (num1,num2);
+add = num1 + num2;
+minus = num1 - num2;
+multiply = num1 * num2;
+devide = num1 / num2;
+alert (`You will get
+        ${num1} + ${num2} = ${add};
+        ${num1} - ${num2} = ${minus};
+        ${num1} * ${num2} = ${multiply};
+        ${num1} / ${num2} = ${devide};`);
